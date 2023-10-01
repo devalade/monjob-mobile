32b7da90616281804a4ae0f1109cf01d8f9609db
Charaf Alade YESSOUFOU <aladecharaf23@gmail.com>
2023-10-01 15:08:05 +0100

    :wrench:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { Slot, SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { SessionProvider } from '../utils/ctx';
+import { TamaguiProvider, Theme } from 'tamagui';
+import config from '../tamagui.config';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -12,7 +19,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'sign-in',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -21,6 +28,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
     ...FontAwesome.font,
   });
 
@@ -46,11 +55,14 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <SessionProvider>
+      <TamaguiProvider config={config}>
+        <Theme name='dark'>
+          <Theme name='green'>
+            <Slot />
+          </Theme>
+        </Theme>
+      </TamaguiProvider>
+    </SessionProvider>
   );
 }
